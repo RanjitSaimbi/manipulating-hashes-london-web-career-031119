75cf3836610e475da60745cb71014ed130caf9d4
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-25 17:47:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,8 @@ def first_challenge
     }
   }
 
- contacts.each do |attribute, value|
-   attribute.each do |attribute, value|
+ contacts.each do |person, data|
+   data.each do |attribute, value|
      if attribute == :favorite_icecream_flavors
        value.delete_if do |flavor|
          flavor == "strawberry"
